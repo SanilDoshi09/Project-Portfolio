1b8bc547816b13de6c923ba0b43c23992718d9a2
Sanil Doshi <goatdev2001@gmail.com>
2024-01-16 00:27:38 -0800

    integrated custom logging

--- analysis/scripts/main.py
+++ analysis/scripts/main.py
@@ -1,6 +1,13 @@
 import os
+import logging
+
+from logger import set_logger
 
 def main():
+    # set logger
+    logger = set_logger("main")
+    
+    logger.info("testing")
     print("testing")
 
 if __name__ == "__main__":
